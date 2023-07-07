83b331a6a6a2005627cf1f30b8babb3d59f64e92
white4446 <138792872+white4446@users.noreply.github.com>
2023-07-07 11:09:19 +0100

    Update main.js
    Update main.js by white

---
+++ main.js
@@ -0,0 +1,12 @@
+// Generate a random number between 1 and 10
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+
+// Print the random number to the console
+console.log("Random number:", randomNumber);
+
+// Check if the number is even or odd
+if (randomNumber % 2 === 0) {
+  console.log("The number is even.");
+} else {
+  console.log("The number is odd.");
+}
